bcf73d5814859b3532c9fdfce7cb5a1cade74664
josecostamartins <josecostamartins@gmail.com>
2016-07-22 04:47:22 -0300

    feat: Initial Commit
    This is the first commit, this android application only lists the
    apps that will be built during udacity android developer nanodegree
    and shows a toast of the selected project

---
+++ app/src/main/java/br/com/digitaldreams/portfolioapp/MainActivity.java
@@ -0,0 +1,64 @@
+package br.com.digitaldreams.portfolioapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnClickListener {
+
+    Button popularMoviesButton;
+    Button stockHawkButton;
+    Button buildItBiggerButton;
+    Button makeYourAppMaterialButton;
+    Button goUbiquitousButton;
+    Button capstoneButton;
+    final Context mContext = MainActivity.this;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        popularMoviesButton = (Button) findViewById(R.id.popular_movies);
+        stockHawkButton = (Button) findViewById(R.id.stock_hawk);
+        buildItBiggerButton = (Button) findViewById(R.id.build_it_bigger);
+        makeYourAppMaterialButton = (Button) findViewById(R.id.make_your_app_material);
+        goUbiquitousButton = (Button) findViewById(R.id.go_ubiquitous);
+        capstoneButton = (Button) findViewById(R.id.capstone);
+
+        popularMoviesButton.setOnClickListener(this);
+        stockHawkButton.setOnClickListener(this);
+        buildItBiggerButton.setOnClickListener(this);
+        makeYourAppMaterialButton.setOnClickListener(this);
+        goUbiquitousButton.setOnClickListener(this);
+        capstoneButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.popular_movies){
+            Toast.makeText(mContext, "This is my \"Popular Movies\" App", Toast.LENGTH_LONG).show();
+        }
+        if (view.getId() == R.id.stock_hawk){
+            Toast.makeText(mContext, "This is my \"Stock Hawk\" App", Toast.LENGTH_LONG).show();
+        }
+        if (view.getId() == R.id.build_it_bigger){
+            Toast.makeText(mContext, "This is my \"Built It Bigger\" App", Toast.LENGTH_LONG).show();
+        }
+        if (view.getId() == R.id.make_your_app_material){
+            Toast.makeText(mContext, "This is my \"Make Your App Material\" App", Toast.LENGTH_LONG).show();
+        }
+        if (view.getId() == R.id.go_ubiquitous){
+            Toast.makeText(mContext, "This is my \"Go Ubiquitous\" App", Toast.LENGTH_LONG).show();
+        }
+        if (view.getId() == R.id.capstone){
+            Toast.makeText(mContext, "This is my \"Capstone\" App", Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
